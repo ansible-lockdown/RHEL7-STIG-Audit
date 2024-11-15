182883a4f1d3348919512cfc906ed4f31272a258
Mark Bolwell <mark.bollyuk@gmail.com>
2024-11-15 08:44:45 +0000

    Updated RuledID stig_v3r15
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -24,7 +24,7 @@
 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=STIG # Benchmark Name aligns to the audit
-BENCHMARK_VER=v3r14
+BENCHMARK_VER=v3r15
 BENCHMARK_OS=RHEL7
 
 # Goss host Variables
